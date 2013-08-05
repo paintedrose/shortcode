d62eee674a4558ac2af8d8dabd2630c28bdae4bd
paintedrose <rory4lyndsay@hotmail.co.uk>
2013-08-05 13:09:47 +0100

    Update paintedrose.php

--- paintedrose.php
+++ paintedrose.php
@@ -2,4 +2,14 @@
 
 add_action('admin_menu', 'paintedrose_admin_actions');
 
+function paintedrose_admin_actions() {
+  add_options_page('PaintedRose_Shortcodes', 'PaintedRose_Shortcodes', 'manage_options', '__FILE__', 'paintedrose_admin');
+}
+
+function paintedrose_admin() {
+  echo 'HELLO FROM GITHUB';
+}
+
+}
+
 ?>
