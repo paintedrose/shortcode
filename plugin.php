07bc201d1b3c97a751b2feab6e2f9d12fea8be8a
paintedrose <rory4lyndsay@hotmail.co.uk>
2013-08-05 12:30:44 +0100

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,7 @@
+<?php
+
+//Lets add our admin page action first!
+//-----------------------------------------------------------------------------------------------
+add_action('admin_menu', 'paintedrose_admin_actions');
+
+?>
